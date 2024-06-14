decae2c3144d4a8ada1151d3b12f961f2945df54
shroukgamall17 <shrouqgamal17@gmail.com>
2024-06-15 02:46:54 +0300

    editing dashboard

--- src/app/components/Admin-Dashboard/admin-dashboard/admin-details/admin-details.component.ts
+++ src/app/components/Admin-Dashboard/admin-dashboard/admin-details/admin-details.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-
+import { ActivatedRoute, Router } from '@angular/router';
+import { IAdmin } from '../../../../models/IAdmin';
+import { AccountService } from '../../../../services/account.service';
 @Component({
   selector: 'app-admin-details',
   standalone: true,
@@ -8,5 +10,20 @@ import { Component } from '@angular/core';
   styleUrl: './admin-details.component.css'
 })
 export class AdminDetailsComponent {
+  currentId!: number;
+  currentAdmin!: IAdmin;
+  constructor( private accountService: AccountService,private router: Router, private route: ActivatedRoute) { }
+  
+  ngOnInit(): void {
+    this.route.params.subscribe((params) => {
+      this.currentId = params['id'];
+    });
+    this.accountService.getAdmin(this.currentId).subscribe((res) => {
+      this.currentAdmin = res;
+    })
+  }
 
+  back(): void {
+    this.router.navigate(['/dashboard/employerDashboard']);
+  }
 }
